966693de98f390ce96de9a1b5b411c0db7efbf7d
Tobias Schmidt <tobias.schmidt@seitenbau.com>
2013-07-19 17:46:15 +0200

    small refactoring, indention

--- multicolumn.js
+++ multicolumn.js
@@ -10,166 +10,167 @@
  * July 2013
  * Tobias Schmidt <tobias.schmidt@seitenbau.com>
  */
-;(function ( $, window, document, undefined ) {
+ ;(function ( $, window, document, undefined ) {
 
-    var pluginName = 'multicolumn',
-        defaults = {
-          columnGap : 15, //is converted into percent in relative mode
-          columnCount: 'auto',
-          wrapperClass : 'column-wrapper',
-          hiddenClass : 'mc-hidden',
-          mode : 'relative',
-          childSelector : false
-        };
+   var pluginName = 'multicolumn',
+      defaults = {
+        columnGap : 15, //is converted into percent in relative mode
+        columnCount: 'auto',
+        wrapperClass : 'column-wrapper',
+        hiddenClass : 'mc-hidden',
+        mode: 'relative',
+        childSelector: false
+      };
 
-    function Plugin( element, options ) {
-        this.jse = element;
-        this.element   = $(element);
-        this.options   = $.extend( {}, defaults, options );
-        this._defaults = defaults;
-        this._name     = pluginName;
-        this.init();
-    }
+   function Plugin( element, options ) {
+      this.jse = element;
+      this.element   = $(element);
+      this.options   = $.extend( {}, defaults, options );
+      this._defaults = defaults;
+      this._name     = pluginName;
+      this.init();
+   }
 
-    Plugin.prototype = {
+   Plugin.prototype = {
 
-      init : function() {
-        this.doColumns();
-        this.setResizeHandler();
-      },
+   init : function() {
+     this.doColumns();
+     this.setResizeHandler();
+   },
 
-      doColumns : function() {
-        var self = this;
-        this.element.each(function() {
-          var $el = $(this),
-              gapWidth = self.options.columnGap,
-              tagName = $el.prop('tagName'),
-              classes = $el.attr('class'),
-              listMarginLeft = $el.css('margin-left'),
-              listMarginRight = $el.css('margin-right'),
-              listMarginBottom = $el.css('margin-bottom'),
-              listMarginTop = $el.css('margin-top'),
-              listPaddingRight = $el.css('padding-right'),
-              listPaddingLeft = $el.css('padding-left'),
-              listPaddingBottom = $el.css('padding-bottom'),
-              listPaddingTop = $el.css('padding-top'),
-              $children = $el.children(self.options.childSelector);
+   doColumns : function() {
+     var self = this;
 
-          // get column Count
-          var columnCount;
-          if(self.options.columnCount === 'auto') {
-            columnCount = $el.css('column-count') ? $el.css('column-count') : $el[0].currentStyle.getAttribute('column-count'); //IE
-          } else {
-            columnCount = self.options.columnCount;
-          }
+     this.element.each(function() {
+       var $el = $(this);
 
-          // check column number
-          if(!columnCount || columnCount < 2) {
-            self.destroy($el);
-            return;
-          }
+       // get column Count
+       var columnCount;
+       if(self.options.columnCount === 'auto') {
+         columnCount = $el.css('column-count') ? $el.css('column-count') : $el[0].currentStyle.getAttribute('column-count'); //IE
+       } else {
+         columnCount = self.options.columnCount;
+       }
 
-          // calculate vars
-          var perColumnItemCount  = Math.floor( $children.length / columnCount ),
-              containerWidth = $el.parent().outerWidth() - (parseInt(listPaddingLeft, 10) + parseInt(listPaddingRight,10)),
-              columnWidth = (containerWidth - (gapWidth * (columnCount - 1))) / columnCount;
+       // check columns
+       if(!columnCount || columnCount < 2) {
+         self.destroy($el);
+         return;
+       }
 
-          if(self.options.mode == 'relative') {
-            columnWidth = (columnWidth / containerWidth * 100) + '%';
-            gapWidth = (gapWidth / containerWidth * 100) + '%';
-          } else {
-            containerWidth = Math.floor(containerWidth);
-            columnWidth = Math.floor(columnWidth);
-          }
+       var gapWidth = self.options.columnGap,
+         tagName = $el.prop('tagName'),
+         classes = $el.attr('class'),
+         listMarginLeft = $el.css('margin-left'),
+         listMarginRight = $el.css('margin-right'),
+         listMarginBottom = $el.css('margin-bottom'),
+         listMarginTop = $el.css('margin-top'),
+         listPaddingRight = $el.css('padding-right'),
+         listPaddingLeft = $el.css('padding-left'),
+         listPaddingBottom = $el.css('padding-bottom'),
+         listPaddingTop = $el.css('padding-top'),
+         $children = $el.children(self.options.childSelector);
 
-          // define wrapper element
-          $wrapper = $('<div class="' + self.options.wrapperClass + '"></div>')
-            .css({
-              'margin-left': listMarginLeft,
-              'margin-right': listMarginRight,
-              'margin-top': listMarginTop,
-              'margin-bottom': listMarginBottom,
-              'padding-right' : listPaddingRight,
-              'padding-left' : listPaddingLeft,
-              'padding-top' : listPaddingTop,
-              'padding-bottom' : listPaddingBottom
-            });
+       // calculate vars
+       var perColumnItemCount  = Math.floor( $children.length / columnCount ),
+         containerWidth = $el.parent().outerWidth() - (parseInt(listPaddingLeft, 10) + parseInt(listPaddingRight,10)),
+         columnWidth = (containerWidth - (gapWidth * (columnCount - 1))) / columnCount;
 
-          // get wrapper element
-          var lists = $wrapper.clone();
+       if(self.options.mode == 'relative') {
+         columnWidth = (columnWidth / containerWidth * 100) + '%';
+         gapWidth = (gapWidth / containerWidth * 100) + '%';
+       } else {
+         containerWidth = Math.floor(containerWidth);
+         columnWidth = Math.floor(columnWidth);
+       }
 
-          // fill each column with list elements
-          for (var i = 0; i < columnCount; i++) {
-            var columnMargin = i > 0 ? gapWidth : 0;
-            var $listItems = $children.clone();
-            var fromCount = parseInt((perColumnItemCount * i), 10);
-            var toCount = parseInt((fromCount + perColumnItemCount), 10);
-            $listItems = $listItems.slice(fromCount, toCount);
+       // define wrapper element
+       $wrapper = $('<div class="clearfix ' + self.options.wrapperClass + '"></div>')
+         .css({
+           'margin-left': listMarginLeft,
+            'margin-right': listMarginRight,
+           'margin-top': listMarginTop,
+           'margin-bottom': listMarginBottom,
+           'padding-right' : listPaddingRight,
+           'padding-left' : listPaddingLeft,
+           'padding-top' : listPaddingTop,
+           'padding-bottom' : listPaddingBottom
+         });
 
-            $list = $('<' + tagName + '/>')
-              .css({
-                'display': 'block',
-                'float': 'left',
-                'width': columnWidth,
-                'margin-right': 0,
-                'margin-left': columnMargin,
-                'padding' : 0
-              })
-              .attr('class', classes);
+       // get wrapper element
+       var lists = $wrapper.clone();
 
-            //wrap lists with wrapper and uls
-            lists.append($list.append($listItems));
-          };
+       // fill each column with list elements
+       for (var i = 0; i < columnCount; i++) {
+         var columnMargin = i > 0 ? gapWidth : 0;
+         var $listItems = $children.clone();
+         var fromCount = parseInt((perColumnItemCount * i), 10);
+         var toCount = parseInt((fromCount + perColumnItemCount), 10);
+         $listItems = $listItems.slice(fromCount, toCount);
 
-          //insert new element, hide old
-          $el.after(lists).hide().addClass(self.options.hiddenClass);
-        });
-      },
+         $list = $('<' + tagName + '/>')
+           .css({
+             'display': 'block',
+             'float': 'left',
+             'width': columnWidth,
+             'margin-right': 0,
+             'margin-left': columnMargin,
+             'padding' : 0
+           })
+          .attr('class', classes);
 
-      setResizeHandler : function() {
-        var self = this;
-        $(window).on('orientationchange pageshow resize', self.waitForFinalEvent(function(e) {
-          var _self = self;
-          self.element.each(function() {
-            $el = $(this);
-            _self.destroy($el, _self.bind(_self.doColumns, _self));
-          });
-          })
-        ).trigger('resize');
-      },
+         //wrap lists with wrapper and uls
+         lists.append($list.append($listItems));
+      };
 
-      waitForFinalEvent : function (func, timeout) {
-        var timeoutID , timeout = timeout || 500;
-        return function () {
-          var scope = this , args = arguments;
-          clearTimeout(timeoutID);
-          timeoutID = setTimeout( function () {
-            func.apply( scope , Array.prototype.slice.call( args ) );
-          } , timeout );
-        }
-      },
+       //insert new element, remove old
+       $el.after(lists).hide().addClass(self.options.hiddenClass);
+     });
+   },
 
-      destroy : function ($el, callback) {
-        $el.show().removeClass(this.options.hiddenClass);
-        $el.next('.' + this.options.wrapperClass).remove();
+   setResizeHandler : function() {
+     var self = this;
+     $(window).on('orientationchange pageshow resize', self.waitForFinalEvent(function(e) {
+       var _self = self;
+       self.element.each(function() {
+         $el = $(this);
+         _self.destroy($el, _self.bind(_self.doColumns, _self));
+       });
+     })).trigger('resize');
+   },
 
-        if (typeof callback == 'function') {
-          callback.call();
-        }
-      },
+   waitForFinalEvent : function (func, timeout) {
+     var timeoutID , timeout = timeout || 400;
+     return function () {
+       var scope = this , args = arguments;
+       clearTimeout(timeoutID);
+       timeoutID = setTimeout( function () {
+         func.apply( scope , Array.prototype.slice.call( args ) );
+       } , timeout );
+     }
+   },
 
-      bind : function(fn, scope) {
-        return function () {
-          fn.apply(scope, arguments);
-        };
-      }
+   destroy : function ($el, callback) {
+     $el.show().removeClass(this.options.hiddenClass);
+     $el.next('.' + this.options.wrapperClass).remove();
 
-    };
+     if (typeof callback == 'function') {
+       callback.call();
+     }
+   },
 
-    $.fn[pluginName] = function ( options ) {
-      return this.each(function () {
-        $.data(this, "plugin_" + pluginName, new Plugin( this, options ));
-      });
-    };
+   bind : function(fn, scope) {
+     return function () {
+       fn.apply(scope, arguments);
+     };
+   }
 
-})( jQuery, window, document );
+   };
+
+   $.fn[pluginName] = function ( options ) {
+     return this.each(function () {
+       $.data(this, "plugin_" + pluginName, new Plugin( this, options ));
+     });
+   };
+
+ })( jQuery, window, document );
